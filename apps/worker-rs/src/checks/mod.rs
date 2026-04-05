f46d2d56dd00ea5a3e68a316d85806722da47b6d
lasse <77295879+lassejlv@users.noreply.github.com>
2026-04-05 19:27:10 +0200

    Remove rust worker

--- apps/worker-rs/src/checks/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod external_status;
-pub mod http;
-pub mod ping;
-pub mod tcp;
--- apps/worker-rs/src/main.rs
+++
@@ -1,6 +0,0 @@
-use worker_rs::run;
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    run().await
-}
