bd9eb9cfeedcbca06a24138d88f29214b9fd8160
Lasse <77295879+lassejlv@users.noreply.github.com>
2026-04-24 12:16:27 +0200

    Remove redundant database indexes

---
+++ packages/db/prisma/migrations/20260424101443_remove_redundant_indexes/migration.sql
@@ -0,0 +1,20 @@
+-- DropIndex
+DROP INDEX "api_key_keyHash_idx";
+
+-- DropIndex
+DROP INDEX "external_service_component_externalServiceId_idx";
+
+-- DropIndex
+DROP INDEX "maintenance_window_status_idx";
+
+-- DropIndex
+DROP INDEX "monitor_active_idx";
+
+-- DropIndex
+DROP INDEX "monitor_dependency_monitorId_idx";
+
+-- DropIndex
+DROP INDEX "status_page_monitor_statusPageId_idx";
+
+-- DropIndex
+DROP INDEX "status_page_subscriber_statusPageId_idx";
